faf94f6cb03a8914ff70bec6bef9b9bfd3b8498b
DemonWav <demonwav@gmail.com>
2016-01-23 20:38:50 -0600

    Add obligatory copyright comment

--- src/main/java/com/destroystokyo/paperclipmavenplugin/GenerateDataMojo.java
+++ src/main/java/com/destroystokyo/paperclipmavenplugin/GenerateDataMojo.java
@@ -1,3 +1,12 @@
+/*
+ * Paperclip Maven Plugin - Generates the Data class + patch file for Paperclip
+ *
+ * Copyright (c) 2016 Kyle Wood (DemonWav)
+ * https://github.com/PaperSpigot/Paper
+ *
+ * MIT License
+ */
+
 package com.destroystokyo.paperclipmavenplugin;
 
 import com.sun.codemodel.ClassType;
